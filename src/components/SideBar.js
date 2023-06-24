b9a9619088d14a5f46a76884721afd60af2a3680
sugi04 <sugishivam8@gmail.com>
2023-06-24 16:59:58 +0530

    new changes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -35,13 +35,13 @@ function SideBar() {
     <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Custom Components:</h6>
-            <a className="collapse-item" href="buttons.html">Buttons</a>
-            <a className="collapse-item" href="cards.html">Cards</a>
+            <a className="collapse-item" href="javascript(void)javascript(void)">Buttons</a>
+            <a className="collapse-item" href="javascript(void)">Cards</a>
         </div>
     </div>
 </li>
 <li className="nav-item">
-    <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+    <a className="nav-link collapsed" href="javascript(void)" data-toggle="collapse" data-target="#collapseUtilities"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i className="fas fa-fw fa-wrench"></i>
         <span>Utilities</span>
@@ -50,10 +50,10 @@ function SideBar() {
         data-parent="#accordionSidebar">
         <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Custom Utilities:</h6>
-            <a className="collapse-item" href="utilities-color.html">Colors</a>
-            <a className="collapse-item" href="utilities-border.html">Borders</a>
-            <a className="collapse-item" href="utilities-animation.html">Animations</a>
-            <a className="collapse-item" href="utilities-other.html">Other</a>
+            <a className="collapse-item" href="javascript(void)">Colors</a>
+            <a className="collapse-item" href="javascript(void)">Borders</a>
+            <a className="collapse-item" href="javascript(void)">Animations</a>
+            <a className="collapse-item" href="javascript(void)">Other</a>
         </div>
     </div>
 </li>
@@ -65,7 +65,7 @@ function SideBar() {
 </div>
 
 <li className="nav-item">
-    <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+    <a className="nav-link collapsed" href="javascript(void)" data-toggle="collapse" data-target="#collapsePages"
         aria-expanded="true" aria-controls="collapsePages">
         <i className="fas fa-fw fa-folder"></i>
         <span>Pages</span>
@@ -73,25 +73,25 @@ function SideBar() {
     <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
         <div className="bg-white py-2 collapse-inner rounded">
             <h6 className="collapse-header">Login Screens:</h6>
-            <a className="collapse-item" href="login.html">Login</a>
-            <a className="collapse-item" href="register.html">Register</a>
-            <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
+            <a className="collapse-item" href="javascript(void)">Login</a>
+            <a className="collapse-item" href="javascript(void)">Register</a>
+            <a className="collapse-item" href="javascript(void)">Forgot Password</a>
             <div className="collapse-divider"></div>
             <h6 className="collapse-header">Other Pages:</h6>
-            <a className="collapse-item" href="404.html">404 Page</a>
-            <a className="collapse-item" href="blank.html">Blank Page</a>
+            <a className="collapse-item" href="javascript(void)">404 Page</a>
+            <a className="collapse-item" href="javascript(void)">Blank Page</a>
         </div>
     </div>
 </li>
 
 <li className="nav-item">
-    <a className="nav-link" href="charts.html">
+    <a className="nav-link" href="javascript(void)">
         <i className="fas fa-fw fa-chart-area"></i>
         <span>Charts</span></a>
 </li>
 
 <li className="nav-item">
-    <a className="nav-link" href="tables.html">
+    <a className="nav-link" href="javascript(void)">
         <i className="fas fa-fw fa-table"></i>
         <span>Tables</span></a>
 </li>
